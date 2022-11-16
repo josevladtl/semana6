cc4720e86b93a53c0a61df3813efcf646c85d83b
Jose tandayamo <josevladim2014@gmail.com>
2022-11-16 04:36:11 -0500

    Agregar archivos de proyecto.

---
+++ semana6/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace semana6
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "http://192.168.0.109/clientes/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<semana6.Ws.AccesoDatos> _post;
+        public MainPage()
+        {
+            InitializeComponent();
+            get();
+        }
+
+       public async void get()
+        {
+
+            var content = await client.GetStringAsync(Url);
+            List<semana6.Ws.AccesoDatos> posts = JsonConvert.DeserializeObject<List<semana6.Ws.AccesoDatos>>(content);
+            _post = new ObservableCollection<Ws.AccesoDatos>(posts);
+            MyListView.ItemsSource = _post;
+        }
+
+        private async void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new VentanaIngreso());
+        }
+    }
+}
---
+++ semana6/Ws/AccesoDatos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace semana6.Ws
+{
+    internal class AccesoDatos
+    {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+
+        public string apellido { get; set; }
+
+        public int edad { get; set; }
+    }
+}
